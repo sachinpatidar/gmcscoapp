db9691e109727f5674a82c62c1058b050d46a8a3
sachin\sachinpatidar <sachin.p@cisinlabs.com>
2016-10-15 09:48:48 +0530

    maincontroller

---
+++ www/app/controllers/main/main.controller.js
@@ -0,0 +1,11 @@
+﻿angular.module('main.module.controller', []).controller('main', function ($scope, $state, httpServices, $ionicLoading) {
+    httpServices.get('/GetCategoryList/L').then(function (response) {
+       
+
+        $scope.values = response.data.GetCategoryListResult;
+    }, function (error) {
+
+
+    })
+
+})
