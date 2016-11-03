7e59e06322be0e641390e8693d799bb641787cbc
sachin patidar <sachin.p@cisinlabs.com>
2016-11-03 21:33:44 +0530

    change in profile pi8c

--- www/app/controllers/addblog/addblog.controller.js
+++ www/app/controllers/addblog/addblog.controller.js
@@ -20,6 +20,32 @@
     }
     $scope.addBlog = function (data) {
       
+        document.addEventListener("deviceready", onDeviceReady, false);
+        function onDeviceReady() {
+            for (var i = 0; i < $scope.images.length; i++) {
+
+
+                var fileURL = $scope.images[i];
+                var options = new FileUploadOptions();
+                options.fileKey = "file";
+                options.fileName = fileURL.substr(fileURL.lastIndexOf('/') + 1);
+                options.mimeType = "text/plain";
+                var ft = new FileTransfer();
+                $ionicLoading.show();
+                if (fileURL == null || fileURL == "") {
+
+                }
+                ft.upload(fileURL, encodeURI("http://smartservicesapp.com/BlogImagesUpload.ashx"), function (r) {
+                    JSON.stringify(r);
+                   
+                }, function (error) {
+                    alert("An error has occurred: Code = " + error.code);
+                    alert("upload error source " + error.source);
+                    alert("upload error target " + error.target);
+                }, options);
+            }
+        }
+
         var result={blog:data,image:$scope.images}
         localStorage.setItem("blogadded", JSON.stringify(result));
         var value = $ionicHistory.clearCache();
--- www/app/controllers/login/login.controller.js
+++ www/app/controllers/login/login.controller.js
@@ -14,14 +14,21 @@
                     $rootScope.loginStatus = true;
                     alert(JSON.stringify(response));
                     $ionicHistory.clearHistory();
-                    $state.go('dashboard');
+                    ionicToast.show(response.data.Success, 'bottom', false, 2500);
+
+                    httpServices.get('/GetUserInfo/' + response.data.Source).then(function (dat) {
+                        alert(JSON.stringify(dat));
+                        $rootScope.profilePicture ="http://smartservicesapp.com/Uploads/profilepic/"+ dat.data.GetUserInfoResult[0].FilePathName;
+                        $rootScope.profileName = "Hello! " + dat.data.GetUserInfoResult[0].FirstName;
+                        alert("Hello! " + dat.data.GetUserInfoResult[0].FirstName)
+                        $state.go('dashboard');
+                    }, function (error) {
+
+                    })
+                   
                 }
                 else {
-                    ionicToast.show(response.data.Success, 'bottom', false, 2500);
-                    $scope.$apply(function () {
-                        $scope.data.UserName = '';
-                        $scope.data.Password = '';
-                    });
+                    
                     
                 }
             }, function (error) {
